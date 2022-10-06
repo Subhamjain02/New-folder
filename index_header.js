8609074290a8c536e709f8ac0c934c077cb83f13
Jagannath Pandit <99jagannath@gmail.com>
2022-10-06 23:56:01 +0530

    fix login modal

--- index_header.js
+++ index_header.js
@@ -96,14 +96,8 @@ async function fetch_navbar() {
                 <label class="form-label" for="typePasswordY">Confirm Password</label>
               </div>
 
-              <p class="small mb-4 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
 
               <button class="btn btn-outline-light btn-lg px-5 mb-3" type="submit" onclick="signup()">Sign Up</button>
-              <div class="d-flex justify-content-center text-center mt-1 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-              </div>
 
             </div>
 
@@ -142,14 +136,10 @@ async function fetch_navbar() {
                 <label class="form-label" for="typePasswordZ">Password</label>
               </div>
 
-              <p class="small mb-4 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+              
 
               <button class="btn btn-outline-light btn-lg px-5" type="submit" onclick="signin()">Login</button>
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-              </div>
+              
 
             </div>
 
